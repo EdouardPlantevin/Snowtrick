992cb114fb280717c235f8f36647285a3a0c9644
Edouard_Plantevin <plantevin.contact@gmail.com>
2022-04-01 10:55:28 +0200

    [UPDATE] Homepage dynamic

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(TrickRepository $trickRepository): Response
     {
-        return $this->render('homepage/index.html.twig');
+        $tricks = $trickRepository->findAll();
+        return $this->render('homepage/index.html.twig', [
+            'tricks' => $tricks
+        ]);
     }
 }
